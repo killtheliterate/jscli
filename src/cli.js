df8f5fcedfcbf2bff76764199e34dfbdca705a14
talon <tpoole@victorops.com>
2016-01-14 10:19:11 -0700

    Add version

--- src/cli.js
+++ src/cli.js
@@ -4,8 +4,11 @@ import yargs from 'yargs'
 
 import init from './commands/init'
 
+import {version} from '../package.json'
+
 yargs
   .usage('\nUsage: js <command>')
+  .version(version)
   .command('init', 'Initialize a new JavaScript project', init)
   .demand(1)
   .help('h')
